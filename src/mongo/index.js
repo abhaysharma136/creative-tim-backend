f732e04322bcbeef20f8c43302868b1ac470a053
Abhay Sharma <abhaysharmajr@gmail.com>
2023-04-11 16:04:50 +0530

    creative-tim-backend

---
+++ src/mongo/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const dbConnect = () => {
+  mongoose.connection.once("open", () => console.log("DB connection"));
+  return mongoose.connect(`mongodb+srv://${process.env.DB_LINK}`, {
+    keepAlive: true,
+  });
+};
